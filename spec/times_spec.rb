2378e147d1a499eb039a9ab26db2c5d1b15c2fb8
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-02 22:18:28 +0000

    Done.

--- spec/times_spec.rb
+++ spec/times_spec.rb
@@ -1,10 +1,14 @@
 require_relative "spec_helper"
 require_relative "../times.rb"
 
-describe "#using_times" do 
-	it "puts out the phrase 'Wingardium Leviosa' 7 times by using the `times` construct" do
+describe "#using_times" do
+	it 7.times do
+		puts "Wingardium Leviosa"
+	end
 
+
+end
 	looping_string = "Wingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\nWingardium Leviosa\n"
-	expect{ using_times }.to output(looping_string).to_stdout 
+	expect{ using_times }.to output(looping_string).to_stdout
 	end
 end
